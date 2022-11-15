5bf9237a25b893a38452244332348b200837179d
kbdaine <kbpunzalan@up.edu.ph>
2022-11-15 21:19:42 +0800

    fix: handle duplicates

---
+++ k.py
@@ -0,0 +1,3 @@
+symbol_table = [{"1":1}, {"1":2}, {"1": 3}]
+
+print(symbol_table[0]["1"])
--- lexical_analyzer.py
+++ lexical_analyzer.py
@@ -12,9 +12,21 @@ def input_file(file_name):
 
   return lines
 
+def has_duplicate(lexeme):
+  in_symbol_table = False
+  for item in symbol_table:
+    if item['lexeme'] == lexeme:
+      return not in_symbol_table
+
+  return in_symbol_table
+
+
 def lexeme_match(symbol_table, i, lexeme, type):
-  if re.search(rf"\b{lexeme}\b", i): # f: formatted string; r: regex pattern
-    symbol_table.append({ "lexeme": lexeme, "type": type })
+  word = rf"\b{lexeme}\b" # f: formatted string; r: regex pattern
+  if re.search(word, i):
+    in_symbol_table = has_duplicate(lexeme)
+    if not in_symbol_table:
+      symbol_table.append({ "lexeme": lexeme, "type": type })
     
 lines = input_file('files/input.lol')
 keywords = input_file('files/keywords.txt') 
@@ -25,9 +37,5 @@ for line in lines:
   for keyword in keywords:
     lexeme_match(symbol_table, line, lexeme=rf"{keyword}", type="keyword")
 
-
 for symbol in symbol_table:
   print(symbol)
-
-
-# print(lexemes)
